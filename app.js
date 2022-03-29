5e860b52824de5d44795a7f783aa10be6478f4b9
Dan <en07djc@icloud.com>
2022-03-29 11:29:10 +0100

    Setting up basic maths functions that work with two numbers.

---
+++ app.js
@@ -0,0 +1,24 @@
+// Add
+const add = function(num1, num2) {
+	let result = num1 + num2
+  return result;
+};
+
+// Subtract
+const subtract = function(num1, num2) {
+	let result = num1 - num2
+  return result;
+};
+
+// Multiply
+const multiply = function(num1, num2) {
+	let result = num1 * num2
+  return result;
+};
+
+// Divide
+const divide = function(num1, num2) {
+	let result = num1 / num2
+  return result;
+};
+
